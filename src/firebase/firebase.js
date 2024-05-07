dc591ddb8955a5efbaeae4b201b837e8789924d5
SoomIn <163857590+tnals0924@users.noreply.github.com>
2024-05-07 18:16:50 +0900

    feat: Firebase DB 연동

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile, signOut } from 'firebase/auth';
+import { getDatabase, ref, get, update, set } from "firebase/database";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,11 +20,17 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getDatabase(app);
 
 export {
   auth,
+  db,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
   updateProfile,
   signOut,
+  ref,
+  get,
+  update,
+  set
 };
