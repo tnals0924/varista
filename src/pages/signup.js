3d73ef11250da5778a3e6935a2faf750bba88371
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2024-05-16 20:40:07 +0900

    chore: 에러메시지 위치 변경

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -3,30 +3,30 @@ import toast from 'react-hot-toast';
 import { auth, createUserWithEmailAndPassword, updateProfile } from '../firebase/firebase';
 import { useNavigate } from 'react-router-dom';
 
-const getErrorMessage = (error) => {
-    switch (error.code) {
-        case "auth/email-already-in-use":
-            return '이미 존재하는 계정의 이메일입니다.';
-        case "auth/internal-error":
-            return 'Firebase 인증서버가 응답하지 않습니다.';
-        case "auth/invalid-email":
-            return '잘못된 이메일 형식입니다.';
-        case "auth/invalid-password":
-            return '비밀번호는 6글자 이상 입력해야 합니다.';
-        case "auth/network-request-failed":
-            return '네트워크 연결에 실패했습니다.';
-        default:
-            console.log(error.code);
-            return '알 수 없는 오류가 발생했습니다.';
-    }
-}
-
 const Signup = () => {
     const navigate = useNavigate();
     const name = useRef(null);
     const email = useRef(null);
     const password = useRef(null);
     const password_check = useRef(null);
+
+    const getErrorMessage = (error) => {
+        switch (error.code) {
+            case "auth/email-already-in-use":
+                return '이미 존재하는 계정의 이메일입니다.';
+            case "auth/internal-error":
+                return 'Firebase 인증서버가 응답하지 않습니다.';
+            case "auth/invalid-email":
+                return '잘못된 이메일 형식입니다.';
+            case "auth/invalid-password":
+                return '비밀번호는 6글자 이상 입력해야 합니다.';
+            case "auth/network-request-failed":
+                return '네트워크 연결에 실패했습니다.';
+            default:
+                console.log(error.code);
+                return '알 수 없는 오류가 발생했습니다.';
+        }
+    }
 
     const handleSignup = async() => {
         if (name.current.value == null || name.current.value === "") {
@@ -57,8 +57,8 @@ const Signup = () => {
         toast.promise(
             createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
                 .then(async (credential) => {
-                    await updateProfile(credential.user, { displayName: name.current.value })
-                    navigate('/login')
+                    await updateProfile(credential.user, { displayName: name.current.value });
+                    navigate('/login');
                 }),
             {
                 loading: '회원가입 진행 중입니다...',
@@ -86,7 +86,11 @@ const Signup = () => {
                         <input className="user_input" type="password" ref={password}></input>
                         <label className="label">비밀번호 확인</label>
                         <input className="user_input" type="password" ref={password_check}></input>
-                        <button className="button mt-[72px] text-shadow shadow-white" onClick={handleSignup}>가입하기</button>
+                        <button className="button mt-[72px] text-shadow shadow-white" 
+                                onClick={handleSignup}
+                        >
+                            가입하기
+                        </button>
                     </div>
                 </div>
             </div>
